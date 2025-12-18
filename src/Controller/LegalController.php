5156d280e100e89a8e54415099319e1be9262f99
MIchel Khauv <michelkhauv@gmail.com>
2025-12-18 22:26:04 +0100

    Ajout mention légale

--- src/Controller/LegalController.php
+++ src/Controller/LegalController.php
@@ -11,8 +11,30 @@ final class LegalController extends AbstractController
     #[Route('/legal', name: 'app_legal')]
     public function index(): Response
     {
-        return $this->render('legal/index.html.twig', [
-            'controller_name' => 'LegalController',
-        ]);
+        return $this->render('legal/index.html.twig');
+    }
+
+    #[Route('/legal/cgu', name: 'app_legal_cgu')]
+    public function cgu(): Response
+    {
+        return $this->render('legal/cgu.html.twig');
+    }
+
+    #[Route('/legal/cgv', name: 'app_legal_cgv')]
+    public function cgv(): Response
+    {
+        return $this->render('legal/cgv.html.twig');
+    }
+
+    #[Route('/legal/politique-confidentialite', name: 'app_legal_privacy')]
+    public function privacy(): Response
+    {
+        return $this->render('legal/privacy.html.twig');
+    }
+
+    #[Route('/legal/qui-sommes-nous', name: 'app_legal_about')]
+    public function about(): Response
+    {
+        return $this->render('legal/about.html.twig');
     }
 }
