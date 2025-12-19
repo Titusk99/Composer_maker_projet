59f57323a197a43da0d2e5bc67e881cc0bc9154b
MIchel Khauv <michelkhauv@gmail.com>
2025-12-19 08:39:05 +0100

    Formulaire de contact et envoie vers mailtrap

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Form\ContactForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
+        $form = $this->createForm(ContactForm::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $email = (new Email())
+                ->from($data['email'])
+                ->to('contact@jeu.com')
+                ->subject($data['sujet'])
+                ->text($data['message']);
+
+            $mailer->send($email);
+            return $this->redirectToRoute('app_contact');
+        }
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'contactForm' => $form,
         ]);
     }
 }
--- src/Form/ContactForm.php
+++ src/Form/ContactForm.php
@@ -3,22 +3,53 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ContactForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
-        ;
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            // Configure your form options here
-        ]);
+            ->add('nom', TextType::class, [
+                'label' => 'Votre nom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre nom',
+                    ]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre email',
+                    ]),
+                    new Email([
+                        'message' => 'Veuillez entrer un email valide',
+                    ]),
+                ],
+            ])
+            ->add('sujet', TextType::class, [
+                'label' => 'Sujet',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un sujet',
+                    ]),
+                ],
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Votre message',
+                'attr' => ['rows' => 6],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre message',
+                    ]),
+                ],
+            ]);
     }
 }
